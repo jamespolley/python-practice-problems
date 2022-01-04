cbc525efd2a0490061ca2e35d166e375f6b9b179
James Polley <jmspolley@gmail.com>
2022-01-04 12:52:39 -0600

    Renamed fibonacci, fibonacci_recursive. Added comments.

--- fibonacci_memoized.py
+++ fibonacci_memoized.py
@@ -7,9 +7,9 @@
 import time
 
 
-def fibonacci(n):
+def fibonacci_recursive(n):
   if n < 2: return n
-  return fibonacci(n - 1) + fibonacci(n - 2)
+  return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
 
 memo = {}
 def fibonacci_memoized(n):
@@ -20,16 +20,26 @@ def fibonacci_memoized(n):
   return current
 
 # Comparison Tests
+print("\nComparison Tests\n================")
 for n in (5, 10, 20, 30):
+  print(f"n={n}")
   start = time.perf_counter_ns()
-  output = fibonacci(n)
+  output = fibonacci_recursive(n)
   runtime = time.perf_counter_ns() - start
-  print(f"fibonacci  n={n}")
-  print(f"    output={output}  runtime(ns)={runtime}\n")
+  print(f"fibonacci_recursive  output={output}  runtime(ns)={runtime}")
 
   memo = {}
   start = time.perf_counter_ns()
   output = fibonacci_memoized(n)
   runtime = time.perf_counter_ns() - start
-  print(f"fibonacci_memoized  n={n}")
-  print(f"    output={output}  runtime(ns)={runtime}\n")
+  print(f"fibonacci_memoized  output={output}  runtime(ns)={runtime}")
+  print()
+
+# Observations
+# ============
+# fibonacci_recursive
+#   runtime complexity --> O(ϕ^2), ϕ is about 1.618 (golden ratio)
+#   space complexity ----> O(n)
+# fibonacci_memoized
+#   runtime complexity --> O(n)
+#   space complexity ----> O(n)
