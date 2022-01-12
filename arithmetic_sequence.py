35a553d178e3cb24988d8ed67cc5380af9c5aef7
James Polley <jmspolley@gmail.com>
2022-01-12 13:23:10 -0600

    Began implementation

---
+++ arithmetic_sequence.py
@@ -0,0 +1,74 @@
+
+def generate_arith_seq(a1, d, n):
+  """Generates a list containing a finite arithmetic sequence, given:
+  - value of the 1st term (a1),
+  - common difference (d), AND
+  - index of the final term(n), 1st term being 1
+  ---  
+  Raises an exeption if n is not 1 or greater."""
+  if n < 1:
+    raise Exception("n must be 1 or greater.")
+  return [a1 + d*n for n in range(n)]
+print(generate_arith_seq(1, -2, 5))
+
+def arith_seq_find_vars(arith_seq):
+  """Given a list containing a finite arithmetic sequence, finds the variables:
+  - value of the final term (an),
+  - value of the 1st term (a1),
+  - common difference (d), AND
+  - index of the final term (n), 1st term being 1
+  ---
+  Raises an exception if arithmetic sequence is invalid."""
+  d = arith_seq[1] - arith_seq[0]
+  # IN PROGRESS
+
+def arith_seq_solve_unknown(an=None, a1=None, d=None, n=None):
+  """Given that the other variables are known, solves for:
+  - value of the nth term (an),
+  - value of the 1st term (a1),
+  - common difference (d), OR
+  - index of term n (n), 1st term being 1
+  ---
+  Raises an exception if too few known variables are given."""
+  known_vars = sum([True for arg in (an, a1, d, n) if arg is not None])
+  if known_vars < 3:
+    raise Exception("Three known variables are required")  
+  if an is None: # Solve for an
+    return a1 + (d * (n - 1))
+  if a1 is None: # Solve for a1
+    return an // (d * (n - 1))
+  if d is None: # Solve for d
+    return (an - a1) // (n - 1)
+  if n is None: # Solve for n
+    return ((an - a1) // d) + 1
+
+
+# Tests
+if __name__ == "__main__":
+  print("\nTESTS\n")
+
+  print("generate_arith_seq")
+  print("==================")
+  a1 = 5
+  d = -3
+  n = 8
+  print(f"Variables: a1={a1}, d={d}, n={n}")
+  print("  Result:", generate_arith_seq(a1, d, n), "\n")
+
+
+  print("arith_seq_solve_unknown")
+  print("=======================")
+  an = 15
+  a1 = 3
+  d = 3
+  n = 5
+  print("Given the arithmetic sequence: {3, 6, 9, 12, 15...}")
+  print(f"Variables: an={an}, a1={a1}, d={d}, n={n}")
+  result = arith_seq_solve_unknown(a1=a1, d=d, n=n)
+  print(f"  When an is unknown, result: an={result}")
+  result = arith_seq_solve_unknown(an=an, d=d, n=n)
+  print(f"  When a1 is unknown, result: a1={result}")
+  result = arith_seq_solve_unknown(an=an, a1=a1, n=n)
+  print(f"  When d is unknown, result: d={result}")
+  result = arith_seq_solve_unknown(an=an, a1=a1, d=d)
+  print(f"  When n is unknown, result: n={result}\n")
